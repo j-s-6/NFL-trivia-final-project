c6c6316d4399e88033cd47e1be517411bfa0afd4
shreyathum <srt348@stern.nyu.edu>
2025-08-07 19:42:53 -0400

    test 2

--- app/questions.py
+++ app/questions.py
@@ -2,29 +2,48 @@ from app.team_data import get_teams
 from app.team_data import variables
 
 #import all lists 
+from app.team_data import list_name
 from app.team_data import list_alias
 from app.team_data import list_market
-from app.team_data import list_name
-from app.team_data import list_founded
 from app.team_data import list_owner
 from app.team_data import list_general_manager
 from app.team_data import list_president
 from app.team_data import list_mascot
-from app.team_data import list_championships_won
-from app.team_data import list_playoff_appearances
 from app.team_data import list_venue_name
 
 
 import random 
 
-questions = 0
 
-while questions < 6: 
-    selected_variable = random.choice(variables)
-    selected_team = random.choice(list_name)
+random_teams = random.sample(range(0, 32), 5)
 
-    print(f"What is the ",selected_variable,"for ",selected_team,"?")
-    quetions = questions + 1
+for team in random_teams: 
+    random_variables = random.randint(0, 7)
+    if random_variables == 1:
+        print("What is the alias of the", list_name[team])
+    if random_variables == 2: 
+        print("What is the market of the", list_name[team])
+    if random_variables == 3: 
+        print("Who is the owner of the", list_name[team])
+    if random_variables == 4: 
+        print("Who is the general owner of the", list_name[team])
+    if random_variables == 5: 
+        print("Who is the president of the", list_name[team])
+    if random_variables == 6: 
+        print("What is the mascot of the", list_name[team])
+    if random_variables == 7: 
+        print("What is the venue of the", list_name[team])
+   
+
+
+#questions = 0
+
+#while questions < 6: 
+    #selected_variable = random.choice(variables)
+    #selected_team = random.choice(list_name)
+
+    #print(f"What is the ",selected_variable,"for ",selected_team,"?")
+    #quetions = questions + 1
 
 
 
